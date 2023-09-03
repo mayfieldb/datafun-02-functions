20bf7df37e6354126bc034827b30b34f3870d25f
Betsy Mayfield <s567724@nwmissouri.edu>
2023-09-03 15:31:06 -0500

    Bigfoot Sightings

--- numeric_weather_series.py
+++ numeric_weather_series.py
@@ -119,3 +119,96 @@ if __name__ == "__main__":
         logger.info(f"Median: {object.median()}")
         logger.info("------------------")
 
+
+#Find data of Bigfoot sightings in Mark Twain National Forest
+from numeric_series import NumericSeries
+
+from util_logger import setup_logger
+logger, logname = setup_logger(__file__)
+
+
+class NumericBigfootSeries(NumericSeries):
+    """
+    A class representing a numeric series customized for Bigfoot sightings data.
+
+    (Additional) Attributes:
+        location (string): the location where the data was collected
+    """
+
+    def __init__(self, name, data, location):
+        """
+        Initialize the object when first created using the arguments passed in.
+        Every class needs an __init__ method to construct a new object.
+
+        @args:
+            self (object): the object being created that will hold the real data
+            name (str): a short name for this list of numeric data
+            units (str): the units in which the data is measured
+            data (list): the list of numeric data to be held by the object
+            location (str): the location where the data was collected
+
+        """
+
+        # First, initialize the parent (super) class with parent's attributes
+        # By calling the super constructor method
+        super().__init__(name, data, data2)
+
+        # Then, initialize our additional specialized attributes
+        self.location = location
+
+    def __str__(self):
+        """
+        Return a string representation of the instantiated object.
+        The two underscores before and after indicate this is a special method.
+        Every class needs a __str__ method that returns a string representation of the object.
+        Be sure to use self.attribute_name so you use the object's attribute, not a local variable! 
+
+        Returns:
+            str: a string representation of the instantiated object
+        """
+        str = f"NumericBigfootSeries with name={self.name}, location={self.location}, and {len(self.data)} data points: {self.data}"
+        return str
+
+if __name__ == "__main__":
+
+    name1 = "1970's Sightings"
+    data1 = [1975, 1977, 1978, 1971]
+    loc = "Mark Twain National Forest"
+
+    object1 = NumericBigfootSeries(name1, data1, loc)
+
+  
+    name2 = "1980's Sightings"
+    data2 = [1982, 1989, 1989, 1982]
+    loc2 = "Mark Twain National Forest"
+
+    object2 = NumericBigfootSeries(name2, data2, loc2)
+
+    
+    # Create another object 
+
+    # use range() to create a list of values from 65 to 78
+    # list() converts the range object to a list
+    # range() is a generator - it creates a sequence of numbers without storing them in memory
+    # The arguments in range are from (inclusive) and to (exclusive)
+    data3 = list(range(70, 80))
+    name3 = "Sightings in the 70's & 80's"
+    loc3 = "Mark Twain National Park"
+
+    object3 = NumericBigfootSeries(name3, data3, loc3)
+
+    # log the objects created
+    logger.info(f"Created: {object1}")
+    logger.info(f"Created: {object2}")
+    logger.info(f"Created: {object3}")
+
+    object_list = [object1, object2, object3]
+
+
+    for object in object_list:
+        logger.info(object)
+        logger.info(f"Count: {object.count()}")
+        logger.info(f"Sum: {object.sum()}")
+        logger.info(f"Mean: {object.mean()}")
+        logger.info(f"Median: {object.median()}")
+        logger.info("------------------")
